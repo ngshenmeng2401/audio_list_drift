ae088685b84175b6be69c13471e2c25630210877
SHENMENG <shenmeng0124@gmail.com>
2023-08-01 22:12:25 +0800

    add playPosition in table and change audio name & audio URL to nullable

--- lib/entity/audio_entity.dart
+++ lib/entity/audio_entity.dart
@@ -1,10 +1,12 @@
 import 'package:drift/drift.dart';
 
+@DataClassName('AudioCourse')
 class AudioEntity extends Table{
 
   IntColumn get audioId => integer().autoIncrement()();
-  TextColumn get audioName => text()();
-  TextColumn get audioURL => text()();
+  TextColumn get audioName => text().nullable()();
+  TextColumn get audioURL => text().nullable()();
   IntColumn get totalLength => integer().nullable()();
   IntColumn get playPosition => integer().nullable()();
+  BoolColumn get isPlaying => boolean().nullable()();
 }
