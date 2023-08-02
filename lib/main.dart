290b79a19f8769634dcd3f915159c71b966ed17c
SHENMENG <shenmeng0124@gmail.com>
2023-08-02 12:42:56 +0800

    change initial route

--- lib/main.dart
+++ lib/main.dart
@@ -25,7 +25,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      initialRoute: "/",
+      initialRoute: AppRouter.bottomNavigationBarWidget,
       onGenerateRoute: AppRouter.generatedRoute,
     );
   }
