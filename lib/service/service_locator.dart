f53906dbe4597f26b955b0a9c251e1b1a2bd35fa
SHENMENG <shenmeng0124@gmail.com>
2023-08-03 09:39:18 +0800

    add register a controller in get it

--- lib/service/service_locator.dart
+++ lib/service/service_locator.dart
@@ -1,5 +1,6 @@
 
 import 'package:audio_player_list_with_drift/db/app_db.dart';
+import 'package:audio_player_list_with_drift/screen/controller/audio_controller.dart';
 import 'package:get_it/get_it.dart';
 
 GetIt getIt = GetIt.instance;
@@ -7,4 +8,6 @@ GetIt getIt = GetIt.instance;
 Future<void> setupServiceLocator() async {
 
   getIt.registerLazySingleton<AppDb>(() => AppDb());
+  
+  getIt.registerLazySingleton<AudioController>(() => AudioController());
 }
