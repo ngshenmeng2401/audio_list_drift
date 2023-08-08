c1f329b5702b3d8dd9acfc04fd95a99674feb409
SHENMENG <shenmeng0124@gmail.com>
2023-08-08 15:20:53 +0800

    change to get_it configuration

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:audio_player_list_with_drift/db/app_db.dart';
 import 'package:audio_player_list_with_drift/route/app_route.dart';
+import 'package:audio_player_list_with_drift/screen/controller/audio_list_provider_controller.dart';
+import 'package:audio_player_list_with_drift/screen/controller/audio_player_provider_controller.dart';
 import 'package:audio_player_list_with_drift/service/service_locator.dart';
 import 'package:flutter/material.dart';
 import 'package:path/path.dart';
@@ -18,18 +20,25 @@ void main() {
 class MyApp extends StatelessWidget {
   MyApp({super.key});
 
-  final GlobalKey<NavigatorState> _navigatorKey = GlobalKey<NavigatorState>();
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      initialRoute: AppRouter.audioPlayerListWithGetItScreen,
-      onGenerateRoute: AppRouter.generatedRoute,
+    return
+      // MultiProvider(
+      // providers: [
+      //   Provider<AppDb>(create: (context) => AppDb()),
+      //   Provider<AudioListProviderController>(create: (context) => AudioListProviderController(), dispose: (context, controller) => controller.dispose(),),
+      //   ChangeNotifierProvider<AudioPlayerProviderController>(create: (context) => AudioPlayerProviderController()),
+      // ],
+      // child:
+      MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        initialRoute: AppRouter.audioPlayerListWithGetItScreen,
+        onGenerateRoute: AppRouter.generatedRoute,
+      // ),
     );
   }
 }
