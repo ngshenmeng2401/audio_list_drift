50f6584136423b1244c61eb59b541f7645ada355
SHENMENG <shenmeng0124@gmail.com>
2023-08-09 14:07:33 +0800

    use selector to control the state of audio player in audio details screem

--- lib/screen/provider/audio_details_provider_screen.dart
+++ lib/screen/provider/audio_details_provider_screen.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:audio_player_list_with_drift/db/app_db.dart';
 import 'package:audio_player_list_with_drift/route/app_route.dart';
+import 'package:audio_player_list_with_drift/screen/controller/audio_player_provider_controller.dart';
 import 'package:audio_player_list_with_drift/screen/provider/edit_audio_provider_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:drift/drift.dart' as drift;
@@ -25,28 +26,38 @@ class AudioDetailsWithProviderScreen extends StatefulWidget {
 
 class _AudioDetailsWithProviderScreenState extends State<AudioDetailsWithProviderScreen> {
   AudioEntityData? _audioEntityData;
-  final audioPlayer = AudioPlayer();
-  Duration duration = const Duration();
-  Duration position = const Duration();
-  bool? isPlaying = false;
   final StreamController<AudioEntityData> _audioController =
       StreamController<AudioEntityData>();
 
   Stream<AudioEntityData> get _audioStream => _audioController.stream;
 
+  AudioPlayerProviderController? _audioPlayerProviderController;
+
+  @override
+  void setState(VoidCallback fn) {
+    // TODO: implement setState
+    if(mounted){
+      super.setState(fn);
+    }
+  }
+
   @override
   void initState() {
-    getAudioData();
-    // isPlaying = widget.isPlaying;
-    // TODO: implement initState
-    // print("widget.playPosition: ${widget.playPosition}");
     super.initState();
+    getAudioData();
+    Provider.of<AudioPlayerProviderController>(context, listen: false).playerStream();
+    // setState(() {});
+    // Provider.of<AudioPlayerProviderController>(context, listen: false).audioPlayer.positionStream.listen((event) {
+    //   Provider.of<AudioPlayerProviderController>(context, listen: false).position = event.inSeconds.toInt();
+    //   setState(() {
+    //     // print("position: ${audioPlayerController.position}");
+    //   });
+    // });
   }
 
   @override
   void dispose() {
     _audioController.close();
-    audioPlayer.dispose();
     super.dispose();
   }
 
@@ -60,25 +71,11 @@ class _AudioDetailsWithProviderScreenState extends State<AudioDetailsWithProvide
 
       if (_audioEntityData != null) {
         _audioController.sink.add(_audioEntityData!);
-        if (_audioEntityData!.audioURL != null) {
-          await audioPlayer.setUrl(_audioEntityData!.audioURL!);
-        }
       }
-      if (audioPlayer.duration != null) {
-        duration = audioPlayer.duration!;
-      }
+
     } catch (ex) {
       Fimber.e('d;;exception', ex: ex);
     }
-
-    print("duration $duration");
-    setState(() {});
-    audioPlayer.positionStream.listen((event) {
-      position = event;
-      setState(() {
-        print("position: $position");
-      });
-    });
   }
 
   void navigateToEditAudioScreen(int audioId) async {
@@ -87,50 +84,8 @@ class _AudioDetailsWithProviderScreenState extends State<AudioDetailsWithProvide
                 audioId: audioId, backButtonCallback: getAudioData));
   }
 
-  void playAudio(bool isPlay, int playedPosition) {
-    var entity;
-    print("isPlaying before action: $isPlaying");
-    setState(() {
-      if (isPlay) {
-        audioPlayer.pause();
-        isPlaying = false;
-        // entity = AudioEntityCompanion(
-        //   audioId: drift.Value(widget.arguments.audioId),
-        //   playPosition: drift.Value(position.inSeconds.toInt()),
-        //   isPlaying: const drift.Value(false),
-        // );
-      } else {
-        if (playedPosition != 0) {
-          audioPlayer.seek(Duration(seconds: playedPosition));
-        }
-        audioPlayer.play();
-        isPlaying = true;
-        // entity = AudioEntityCompanion(
-        //   audioId: drift.Value(widget.arguments.audioId),
-        //   // playPosition: const drift.Value(0),
-        //   isPlaying: const drift.Value(true),
-        // );
-      }
-    });
-    print("isPlaying after action: $isPlaying");
-    // Provider.of<AppDb>(context, listen: false).updateAudio(entity);
-    // setState(() {
-    //   getAudioData();
-    // });
-    // audioPlayer.onDurationChanged.listen((Duration dd) {
-    //   setState(() {
-    //     duration = dd;
-    //   });
-    // });
-    // audioPlayer.onPositionChanged.listen((Duration dd) {
-    //   setState(() {
-    //     position = dd;
-    //   });
-    // });
-  }
-
   Future<bool> _onWillPop() async {
-    widget.arguments.backButtonCallback();
+    // widget.arguments.backButtonCallback();
     return true;
   }
 
@@ -174,37 +129,80 @@ class _AudioDetailsWithProviderScreenState extends State<AudioDetailsWithProvide
     Provider.of<AppDb>(context).deleteAudio(widget.arguments.audioId);
   }
 
-  Widget _renderAudioSlider(int playedPosition, int totalLength) {
-    print("Position: $position");
+  Widget _renderAudioSlider(AudioPlayerState audioPlayerState, int totalLength) {
+
+    final audioPlayerController = Provider.of<AudioPlayerProviderController>(context, listen: false);
+
+    if(audioPlayerController.currentAudioButtonId == widget.arguments.audioId && audioPlayerState != AudioPlayerState.stop){
+      return Selector<AudioPlayerProviderController, int>(
+          selector: (context, controller) => controller.position,
+          builder: (BuildContext context, position, Widget? child) {
+            return Slider(
+                min: 0.0,
+                max: totalLength.toDouble(),
+                value: position.toDouble() == totalLength.toDouble() ? 0 : position.toDouble(),
+                onChanged: (double value) {
+                  setState(() {
+                    Provider.of<AudioPlayerProviderController>(context, listen: false).audioPlayer.seek(Duration(seconds: value.toInt()));
+                  });
+                });
+          }
+      );
+      // return Consumer<AudioPlayerProviderController>(
+      //     builder: (BuildContext context, audioPlayerProviderController, Widget? child) {
+      //       return Slider(
+      //           min: 0.0,
+      //           max: totalLength.toDouble(),
+      //           value: audioPlayerProviderController.position.toDouble() == totalLength.toDouble() ? 0 : audioPlayerProviderController.position.toDouble(),
+      //           onChanged: (double value) {
+      //             setState(() {
+      //               Provider.of<AudioPlayerProviderController>(context, listen: false).audioPlayer.seek(Duration(seconds: value.toInt()));
+      //             });
+      //           });
+      //     }
+      // );
+    }
     return Slider(
         min: 0.0,
         max: totalLength.toDouble(),
-        value: position.inSeconds.toDouble(),
-        onChanged: (double value) {
-          setState(() {
-            audioPlayer.seek(Duration(seconds: value.toInt()));
-          });
-        });
+        value: 0,
+        onChanged: null
+    );
   }
 
-  Widget _renderPlayDuration(int played, int total) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 9.0),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Text(
-            // "0",
-            _formatDuration(Duration(seconds: position.inSeconds.toInt())),
-            style: const TextStyle(fontSize: 16),
+  Widget _renderPlayDuration(int total) {
+
+    final audioPlayerController = Provider.of<AudioPlayerProviderController>(context, listen: false);
+
+    return Selector<AudioPlayerProviderController, int>(
+        selector: (context, controller) => controller.position,
+        builder: (BuildContext context, position, Widget? child) {
+        return Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 9.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              if(audioPlayerController.currentAudioButtonId != widget.arguments.audioId || audioPlayerController.position == audioPlayerController.duration.inSeconds.toDouble())...{
+                Text(
+                  _formatDuration(const Duration(seconds: 0)),
+                  style: const TextStyle(fontSize: 16),
+                )
+              } else...{
+                Text(
+                  // "0",
+                  _formatDuration(Duration(seconds: position)),
+                  style: const TextStyle(fontSize: 16),
+                ),
+              },
+              Text(
+                // "0",
+                _formatDuration(Duration(seconds: total)),
+                style: const TextStyle(fontSize: 16),
+              ),
+            ],
           ),
-          Text(
-            // "0",
-            _formatDuration(Duration(seconds: total)),
-            style: const TextStyle(fontSize: 16),
-          ),
-        ],
-      ),
+        );
+      }
     );
   }
 
@@ -219,6 +217,10 @@ class _AudioDetailsWithProviderScreenState extends State<AudioDetailsWithProvide
 
   @override
   Widget build(BuildContext context) {
+
+    // _audioPlayerProviderController ??= context.watch<AudioPlayerProviderController>();
+    // _audioPlayerProviderController = Provider.of<AudioPlayerProviderController>(context, listen: false);
+
     double screenHeight = MediaQuery.of(context).size.height;
     double screenWidth = MediaQuery.of(context).size.width;
 
@@ -255,47 +257,63 @@ class _AudioDetailsWithProviderScreenState extends State<AudioDetailsWithProvide
                               const SizedBox(
                                 height: 20,
                               ),
-                              _renderAudioSlider(
-                                  snapshot.data!.playPosition!,
-                                  snapshot.data!.totalLength!),
+                              Selector<AudioPlayerProviderController, CurrentPlayingInfo>(
+                                  selector: (context, controller) => controller.currentPlayingInfo,
+                                  builder: (BuildContext context, info, Widget? child) {
+                                    var audioPlayerState = AudioPlayerState.play;
+
+                                    if (info.audioId == snapshot.data!.audioId) {
+                                      if(info.playerState == AudioPlayerState.play){
+                                        AudioPlayerState.pause;
+                                      }
+                                      audioPlayerState = info.playerState == AudioPlayerState.play ? AudioPlayerState.pause : AudioPlayerState.play;
+
+
+                                    }
+                                  return _renderAudioSlider(
+                                      audioPlayerState,
+                                      snapshot.data!.totalLength!);
+                                }
+                              ),
                               const SizedBox(
                                 height: 20,
                               ),
-                              _renderPlayDuration(
-                                  snapshot.data!.playPosition!,
-                                  snapshot.data!.totalLength!),
+                              _renderPlayDuration(snapshot.data!.totalLength!),
                               const SizedBox(
                                 height: 20,
                               ),
-                              Container(
-                                height: screenHeight * 0.2,
-                                width: screenWidth * 0.2,
-                                decoration: const BoxDecoration(
-                                    color: Colors.blue, shape: BoxShape.circle),
-                                child: !isPlaying!
-                                    ? IconButton(
-                                        icon: const Icon(
-                                          Icons.play_arrow,
-                                          color: Colors.white,
+                              Selector<AudioPlayerProviderController, CurrentPlayingInfo>(
+                                selector: (context, controller) => controller.currentPlayingInfo,
+                                builder: (BuildContext context, info, Widget? child) {
+                                  var iconData = Icons.play_arrow;
+                                  var audioPlayerState = AudioPlayerState.play;
+
+                                  if (info.audioId == snapshot.data!.audioId) {
+                                    if(info.playerState == AudioPlayerState.play){
+                                      AudioPlayerState.pause;
+                                    }
+                                    iconData = info.playerState == AudioPlayerState.play ? Icons.pause : Icons.play_arrow;
+                                    audioPlayerState = info.playerState == AudioPlayerState.play ? AudioPlayerState.pause : AudioPlayerState.play;
+
+                                  }
+                                  return Container(
+                                      height: screenHeight * 0.2,
+                                      width: screenWidth * 0.2,
+                                      decoration: const BoxDecoration(
+                                          color: Colors.blue, shape: BoxShape.circle),
+                                      child: IconButton(
+                                        icon: Icon(
                                           size: 50,
+                                          color: Colors.white,
+                                          iconData,
                                         ),
                                         onPressed: () {
-                                          playAudio(isPlaying!,
-                                              snapshot.data!.playPosition!);
+                                          // print("Bool: ${controller.isPlaying}");
+                                          context.read<AudioPlayerProviderController>().playAudio(audioPlayerState, snapshot.data!.audioURL!, snapshot.data!.audioId!);
                                         },
-                                      )
-                                    : IconButton(
-                                        icon: const Icon(
-                                          Icons.pause,
-                                          color: Colors.white,
-                                          size: 50,
-                                        ),
-                                        onPressed: () {
-                                          playAudio(isPlaying!,
-                                              snapshot.data!.playPosition!);
-                                        },
-                                      ),
-                              ),
+                                      ));
+                                },
+                              )
                             ],
                           );
                         }
@@ -318,8 +336,8 @@ class _AudioDetailsWithProviderScreenState extends State<AudioDetailsWithProvide
 
 class AudioDetailWithProviderScreenArguments {
   int audioId;
-  final Function() backButtonCallback;
+  // final Function() backButtonCallback;
 
   AudioDetailWithProviderScreenArguments(
-      {required this.audioId, required this.backButtonCallback});
+      {required this.audioId});
 }
