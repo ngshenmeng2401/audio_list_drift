4db5acdb1664af19b7a41b90f37fcd3398356b52
SHENMENG <shenmeng0124@gmail.com>
2023-08-09 14:05:12 +0800

    control the state for audio player controller by notifyListeners

--- lib/screen/controller/audio_player_provider_controller.dart
+++ lib/screen/controller/audio_player_provider_controller.dart
@@ -1,14 +1,14 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_fimber/flutter_fimber.dart';
 import 'package:just_audio/just_audio.dart';
 
 enum AudioPlayerState { play, pause, stop }
 
 class CurrentPlayingInfo {
-  int audioId;
-  AudioPlayerState playerState;
+  final int audioId;
+  final AudioPlayerState playerState;
 
-  CurrentPlayingInfo({
+  const CurrentPlayingInfo({
     required this.audioId,
     required this.playerState,
   });
@@ -16,37 +16,168 @@ class CurrentPlayingInfo {
   CurrentPlayingInfo copyWith({int? audioId, AudioPlayerState? playerState}) => CurrentPlayingInfo(audioId: audioId ?? this.audioId, playerState: playerState ?? this.playerState);
 }
 
-class AudioPlayerProviderController {
+class AudioPlayHistoryInfo {
+  int audioId;
+  int pauseAt;
 
+  AudioPlayHistoryInfo(this.audioId, this.pauseAt);
+}
+
+class AudioPlayerProviderController extends ChangeNotifier {
+
+  List<AudioPlayHistoryInfo> audioPlayHistoryList = [];
   final audioPlayer = AudioPlayer();
   Duration duration = const Duration();
-  Duration position = const Duration();
+  int position = 0;
+  int currentAudioButtonId = 0;
+  int seekPosition = 0;
 
-  final _playerState = AudioPlayerState.stop;
-  AudioPlayerState get playerState => _playerState;
-
-  IconData _iconData = Icons.play_arrow;
-  IconData get iconData => _iconData;
-  bool _isPlaying = false;
-  bool get isPlaying => _isPlaying;
-
-  final currentPlayingInfo = ValueNotifier<CurrentPlayingInfo>(
-    CurrentPlayingInfo(
-      audioId: -1,
-      playerState: AudioPlayerState.stop,
-    ),
+  var currentPlayingInfo = const CurrentPlayingInfo(
+    audioId: -1,
+    playerState: AudioPlayerState.stop,
   );
 
-  void playAudioButton(){
+  void playerStream (){
+    audioPlayer.positionStream.listen((event) {
+      position = event.inSeconds.toInt();
+      notifyListeners();
+    });
+  }
 
-    print("Bool: $_isPlaying");
-    if(!_isPlaying){
-      _iconData = Icons.pause;
-      _isPlaying = true;
-    }else{
-      _iconData = Icons.play_arrow;
-      _isPlaying = false;
+  Future<void> initAudioPlayer() async {
+    String audioURL = "https://commondatastorage.googleapis.com/codeskulptor-demos/DDR_assets/Kangaroo_MusiQue_-_The_Neverwritten_Role_Playing_Game.mp3";
+
+    try{
+      await audioPlayer.setUrl(audioURL);
+
+      if(audioPlayer.duration != null){
+        duration = audioPlayer.duration!;
+      }
+      // audioList = await getIt.get<AppDb>().getAudioList();
+      //
+      // if(audioList != null){
+      //   audioPlayHistoryList.clear();
+      //
+      //   audioList?.forEach((element) {
+      //     audioPlayHistoryList.add(AudioPlayHistoryInfo(element.audioId, 0));
+      //   });
+      //
+      //   audioPlayHistoryList.addAll(audioList?.map((e) {
+      //     return AudioPlayHistoryInfo(e.audioId, 0);
+      //   }) ?? []);
+      //
+      //   for (var audioHistory in audioPlayHistoryList) {
+      //     print("Audio position history: ${audioHistory.audioId}");
+      //   }
+      // }
+      playerStream ();
+      notifyListeners();
+
+    }catch(ex){
+      Fimber.e('d;;exception', ex: ex);
     }
+  }
+
+  //Just keep for note
+  void playAudioButton(
+      // void Function(int audioID) onAudioPausing,
+      // void Function(int audioID, Duration duration) onAudioPreparing,
+      // void Function(int audioID) onAudioPlaying,
+      ){
+
+    // 1. pause previous playing audio
+    // onAudioPausing(-1);
+
+    // 2. prepare current audio
+    // onAudioPreparing(-1, Duration(xxx));
+
+    // 3. play current audio
+    // onAudioPlaying(-1);
+    notifyListeners();
+  }
+
+  void playAudio(AudioPlayerState audioPlayerState, String audioURL, int audioId) {
+    print("Current Audio Id: $currentAudioButtonId");
+    print("Audio Id: $audioId");
+
+    try {
+      //to save the latest audio player position
+      // for (var audioHistory in audioPlayHistoryList) {
+      //   if(audioHistory.audioId == currentAudioButtonId) {
+      //     audioHistory.pauseAt = position;
+      //   }
+      // }
+
+      if (audioId == currentAudioButtonId) {
+        if (audioPlayerState == AudioPlayerState.pause) {
+          duration = audioPlayer.duration!;
+           audioPlayer.pause();
+          currentPlayingInfo = CurrentPlayingInfo(
+            audioId: currentAudioButtonId,
+            playerState: AudioPlayerState.pause,
+          );
+        } else if (audioPlayerState == AudioPlayerState.play) {
+          currentPlayingInfo = CurrentPlayingInfo(
+            audioId: currentAudioButtonId,
+            playerState: AudioPlayerState.play,
+          );
+          if (position == 0) {
+            audioPlayer.setUrl(audioURL);
+          }
+            audioPlayer.play();
+          // if(audioPlayer.duration != null){
+            duration = audioPlayer.duration!;
+          // }
+        }
+      } else {
+        currentPlayingInfo = CurrentPlayingInfo(
+          audioId: audioId,
+          playerState: AudioPlayerState.play,
+        );
+        currentAudioButtonId = audioId;
+         audioPlayer.setUrl(audioURL);
+        // for (var audioHistory in audioPlayHistoryList) {
+        //   if(audioHistory.audioId == audioId){
+        //     seekPosition = audioHistory.pauseAt;
+        //   }
+        // }
+        // await audioPlayer.seek(Duration(seconds: seekPosition == duration.inSeconds ? 0 : seekPosition));
+         audioPlayer.play();
+        duration = audioPlayer.duration!;
+        // await Future.delayed(const Duration(milliseconds: 500));
+
+        // print("After Audio Player State: $audioPlayerState");
+        // print("Current Playing Info: ${currentPlayingInfo.playerState}");
+      }
+      // print("Audio position history:");
+      // for (var audioHistory in audioPlayHistoryList) {
+      //   print("${audioHistory.audioId}: ${audioHistory.pauseAt}");
+      // }
+      notifyListeners();
+      print("duration songs: $duration ");
+    } catch (ex) {
+      ("Audio Player $ex");
+    }
+  }
+
+  void updateButtonSongsEnd(int audioId, int totalLength){
+    // print("audioId: $audioId");
+    // print("position: $position");
+    if(position == totalLength){
+      currentPlayingInfo = CurrentPlayingInfo(
+        audioId: audioId,
+        playerState: AudioPlayerState.pause,
+      );
+      position = 0;
+    }
+    return;
     // notifyListeners();
+  }
+
+  @override
+  void dispose() {
+    audioPlayer.dispose();
+
+    super.dispose();
   }
 }
