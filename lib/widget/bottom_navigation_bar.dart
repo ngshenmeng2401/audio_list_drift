8ae189d117dca24186ca8ea7749b797dbc5c4c9e
SHENMENG <shenmeng0124@gmail.com>
2023-08-09 14:06:26 +0800

    ignore the screen

--- lib/widget/bottom_navigation_bar.dart
+++ lib/widget/bottom_navigation_bar.dart
@@ -19,7 +19,7 @@ class _BottomNavigationBarWidgetState extends State<BottomNavigationBarWidget> {
 
     super.initState();
     _tabs = [
-      const AudioListWithProviderScreen(),
+      // const AudioListWithProviderScreen(),
       const AudioListWithGetItScreen(),
     ];
   }
